802fe19d293225c149bcff4d0edc7578edf6410e
andrewnnov <andrewnnov@yandex.ru>
2023-05-27 10:47:08 -0300

    add btn to PO

--- tests/new_user_full_journey.spec.js
+++ tests/new_user_full_journey.spec.js
@@ -1,8 +1,11 @@
 import { test } from "@playwright/test";
-import { ProductsPage } from "../page-objects/ProductPage";
+import { ProductsPage } from "../page-objects/ProductsPage";
 
 test.only("New user full end-to-end test journey", async ({ page }) => {
   const productPage = new ProductsPage(page);
   await productPage.visit();
+  await productPage.addProductToBasket(0);
+  await productPage.addProductToBasket(1);
+  await productPage.addProductToBasket(2);
   await page.pause();
 });
