0a99a40b48fd716d3c5648afd6cfe99347b2e05f
andrewnnov <andrewnnov@yandex.ru>
2023-05-26 22:44:13 -0300

    assert text changes

--- tests/product_page_add_item.spec.js
+++ tests/product_page_add_item.spec.js
@@ -1,6 +1,16 @@
-import { test } from "@playwright/test";
+import { test, expect } from "@playwright/test";
 
 test("Product page Add to Basket", async ({ page }) => {
   await page.goto("localhost:2221");
-  await page.pause();
+  //await page.pause();
+
+  const addTwoBasketButton = page.locator('[data-qa="product-button"]').first();
+  const basketCounter = page.locator('[data-qa="header-basket-count"]');
+
+  await addTwoBasketButton.waitFor();
+  await expect(addTwoBasketButton).toHaveText("Add to Basket");
+  await expect(basketCounter).toHaveText("0");
+  await addTwoBasketButton.click();
+  await expect(addTwoBasketButton).toHaveText("Remove from Basket");
+  await expect(basketCounter).toHaveText("1");
 });
