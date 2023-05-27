e63e4098aec63e8985b0634d7c335eb7b0caaa95
andrewnnov <andrewnnov@yandex.ru>
2023-05-27 11:34:19 -0300

    add new class Navigation

--- page-objects/ProductsPage.js
+++ page-objects/ProductsPage.js
@@ -1,29 +1,24 @@
 import { expect } from "@playwright/test";
+import { Navigation } from "./Navigation";
 
 export class ProductsPage {
   constructor(page) {
     this.page = page;
     this.addButtons = page.locator('[data-qa="product-button"]');
-    this.basketCounter = page.locator('[data-qa="header-basket-count"]');
   }
   visit = async () => {
     await this.page.goto("/");
-  };
-
-  getBacketCount = async () => {
-    await this.basketCounter.waitFor();
-    const text = await this.basketCounter.innerText();
-    return parseInt(text, 10);
   };
 
   addProductToBasket = async (index) => {
     const specificAddButton = this.addButtons.nth(index);
     await specificAddButton.waitFor();
     await expect(specificAddButton).toHaveText("Add to Basket");
-    const basketCountBeforeAdding = await this.getBacketCount();
+    const navigation = new Navigation(this.page);
+    const basketCountBeforeAdding = await navigation.getBacketCount();
     await specificAddButton.click();
     await expect(specificAddButton).toHaveText("Remove from Basket");
-    const basketCountAfterAdding = await this.getBacketCount();
+    const basketCountAfterAdding = await navigation.getBacketCount();
     expect(basketCountAfterAdding).toBeGreaterThan(basketCountBeforeAdding);
   };
 }
