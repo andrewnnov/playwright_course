389288c7bbc8f93547c9bac03d2d679f2aae2653
andrewnnov <andrewnnov@yandex.ru>
2023-05-27 11:20:21 -0300

    check number of product in basket

--- page-objects/ProductsPage.js
+++ page-objects/ProductsPage.js
@@ -4,16 +4,26 @@ export class ProductsPage {
   constructor(page) {
     this.page = page;
     this.addButtons = page.locator('[data-qa="product-button"]');
+    this.basketCounter = page.locator('[data-qa="header-basket-count"]');
   }
   visit = async () => {
     await this.page.goto("/");
+  };
+
+  getBacketCount = async () => {
+    await this.basketCounter.waitFor();
+    const text = await this.basketCounter.innerText();
+    return parseInt(text, 10);
   };
 
   addProductToBasket = async (index) => {
     const specificAddButton = this.addButtons.nth(index);
     await specificAddButton.waitFor();
     await expect(specificAddButton).toHaveText("Add to Basket");
+    const basketCountBeforeAdding = await this.getBacketCount();
     await specificAddButton.click();
     await expect(specificAddButton).toHaveText("Remove from Basket");
+    const basketCountAfterAdding = await this.getBacketCount();
+    expect(basketCountAfterAdding).toBeGreaterThan(basketCountBeforeAdding);
   };
 }
