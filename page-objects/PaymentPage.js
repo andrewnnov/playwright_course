f12fb4b5874a9ebc01f52e99e5703f463de281c3
andrewnnov <andrewnnov@yandex.ru>
2023-05-29 15:40:55 -0300

    input values

--- page-objects/PaymentPage.js
+++ page-objects/PaymentPage.js
@@ -1,13 +1,20 @@
+import { expect } from "@playwright/test";
+
 export class PaymentPage {
   constructor(page) {
     this.page = page;
     this.discountCode = page
       .frameLocator('[data-qa="active-discount-container"]')
       .locator('[data-qa="discount-code"]');
+    this.discountInput = page.getByPlaceholder("Discount code");
   }
 
   activateDiscount = async () => {
     await this.discountCode.waitFor();
+    const code = await this.discountCode.innerText();
+    await this.discountInput.waitFor();
+    await this.discountInput.fill(code);
+    await expect(this.discountInput).toHaveValue(code);
     await this.page.pause();
   };
 }
