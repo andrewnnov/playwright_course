f2358f5cd85528c8bec03f5c604added98d97951
andrewnnov <andrewnnov@yandex.ru>
2023-05-31 14:21:18 -0300

    get login token

--- tests/my_account.spec.js
+++ tests/my_account.spec.js
@@ -1,7 +1,13 @@
 import { test } from "@playwright/test";
 import { MyAccountPage } from "../page-objects/MyAccountPage";
+import { getLoginToken } from "../api-calls/getLoginToken";
 
 test.only("My account using cookie injection", async ({ page }) => {
+  //make a request to get login token
+  const loginToken = await getLoginToken();
+  console.warn({ loginToken });
+
+  //inject the login token into the browser
   const myAccount = new MyAccountPage(page);
   await myAccount.visit();
 });
