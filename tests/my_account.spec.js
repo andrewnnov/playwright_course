93cf65dfa0f77a9b754a2a5036497e86c549a251
andrewnnov <andrewnnov@yandex.ru>
2023-05-31 14:55:26 -0300

    refactor login and pass

--- tests/my_account.spec.js
+++ tests/my_account.spec.js
@@ -1,11 +1,14 @@
 import { test } from "@playwright/test";
 import { MyAccountPage } from "../page-objects/MyAccountPage";
 import { getLoginToken } from "../api-calls/getLoginToken";
+import { adminDetalis } from "../data/userDetails";
 
 test.only("My account using cookie injection", async ({ page }) => {
   //make a request to get login token
-  const loginToken = await getLoginToken();
-  console.warn({ loginToken });
+  const loginToken = await getLoginToken(
+    adminDetalis.username,
+    adminDetalis.password
+  );
 
   //inject the login token into the browser
   const myAccount = new MyAccountPage(page);
