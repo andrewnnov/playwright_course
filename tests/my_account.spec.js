0746ab7dfc7bd5904c0b7a40a16ccc2997926169
andrewnnov <andrewnnov@yandex.ru>
2023-05-31 15:37:36 -0300

    global setup

--- tests/my_account.spec.js
+++ tests/my_account.spec.js
@@ -1,5 +1,3 @@
-import * as dotenv from "dotenv";
-dotenv.config();
 import { test } from "@playwright/test";
 import { MyAccountPage } from "../page-objects/MyAccountPage";
 import { getLoginToken } from "../api-calls/getLoginToken";
